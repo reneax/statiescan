ca4e70ae6ec870d0ee82f448bc8a0ff43238521e
René <144122671+reneax@users.noreply.github.com>
2025-05-13 16:38:16 +0200

    Fixed spelling mistake in package name

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "nl.reneaxs.statiescan"
+        applicationId = "nl.reneax.statiescan"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
