0c8ec80afb909a1a37e2b38ed751a5fd665f8b27
René <144122671+reneax@users.noreply.github.com>
2025-05-15 12:21:04 +0200

    Changed scaffold to DefaultScreenScaffold.

--- lib/src/screens/intro/intro_screen.dart
+++ lib/src/screens/intro/intro_screen.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:statiescan/src/screens/intro/widgets/intro_carousel/intro_carousel.dart';
+import 'package:statiescan/src/widgets/default_screen_scaffold.dart';
 
 class IntroScreen extends StatelessWidget {
   const IntroScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: IntroCarousel());
+    return const DefaultScreenScaffold(
+      showNavigation: false,
+      child: IntroCarousel(),
+    );
   }
 }
