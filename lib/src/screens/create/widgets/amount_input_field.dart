159d8624de1fd19e35b2bb8669b7c579b88943c8
René <144122671+reneax@users.noreply.github.com>
2025-05-20 11:14:52 +0200

    Fixed bugs & made UX better

--- lib/src/screens/create/widgets/amount_input_field.dart
+++ lib/src/screens/create/widgets/amount_input_field.dart
@@ -4,8 +4,13 @@ import 'package:statiescan/src/utils/amount_formatter.dart';
 
 class AmountInputField extends StatelessWidget {
   final TextEditingController editingController;
+  final FocusNode? focusNode;
 
-  const AmountInputField({super.key, required this.editingController});
+  const AmountInputField({
+    super.key,
+    required this.editingController,
+    this.focusNode,
+  });
 
   bool _isValidInput(value) {
     return value != null && AmountFormatter.stringToAmount(value) != null;
@@ -15,6 +20,7 @@ class AmountInputField extends StatelessWidget {
   Widget build(BuildContext context) {
     return TextFormField(
       controller: editingController,
+      focusNode: focusNode,
       keyboardType: const TextInputType.numberWithOptions(decimal: true),
       inputFormatters: [
         FilteringTextInputFormatter.allow(RegExp(r'^\d*[,.]?\d{0,2}')),
